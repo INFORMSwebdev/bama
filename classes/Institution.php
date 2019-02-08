7706aebe8202829fdf244e97a2809d7a02adc71c
David Wirth <david.wirth@informs.org>
2019-02-08 08:32:36 -0500

    updated inst class

--- classes/Institution.php
+++ classes/Institution.php
@@ -10,6 +10,22 @@ class Institution extends AOREducationObject {
     public static $table = "institutions";
     public static $primary_key = "InstitutionId";
 
+    public static function getInstitutions( $active = TRUE, $asObjects = TRUE) {
+        $institutions = [];
+        $db = new pdo_db( "/common/settings/common.ini", "analytics_education_settings");
+        $sql = "SELECT InstitutionId FROM institutions";
+        $sql .= " WHERE Deleted = " . ($active == TRUE) ? "0" : "1";
+        $insts = $db->queryColumn( $sql );
+        foreach( $insts as $inst){
+            if ($asObjects) {
+                $institutions[] = new Institution($inst);
+            }
+            else {
+                $institutions[] = $inst;
+            }
+        }
+        return $institutions;
+    }
 }
 
 
