4a36d51dea501f607729b04a31a13419e1eb4bcd
David Wirth <david.wirth@informs.org>
2019-02-08 10:54:23 -0500

    added init.php and autoloader (includes app classes and /common/classes)

---
+++ init.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dwirth
+ * Date: 2/8/2019
+ * Time: 10:48 AM
+ */
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+session_start();
+$ini = parse_ini_file( "/common/settings/common.ini", TRUE );
+$aes = $ini['analytics_education_settings'];
+define( "ROOT_DIR", $aes['root_dir'] );
+define( "HTML_DIR", ROOT_DIR . $aes['html_dir'] );
+define( "USER_DIR", ROOT_DIR . $aes['user_dir'] );
+define( "SCRIPTS_DIR", ROOT_DIR . $aes['scripts_dir'] );
+define( "IMAGES_DIR", ROOT_DIR . $aes['images_dir'] );
+define( "SETTINGS_DIR", ROOT_DIR . $aes['settings_dir'] );
+define( "CLASSES_DIR", ROOT_DIR . $aes['classes_dir'] );
+//require_once( CLASSES_DIR . "autoload.php");
