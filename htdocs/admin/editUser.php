5ff4afc1c3b369740abc8746a4e47215f5636b4d
David Wirth <david.wirth@informs.org>
2019-02-28 16:15:35 -0500

    completed ADMIN User Edit page

--- htdocs/admin/editUser.php
+++ htdocs/admin/editUser.php
@@ -29,24 +29,78 @@ $content = <<<EOT
 </div>
 <div id="editorPane">
   <form id="userEditForm"> 
+    <input type="hidden" name="UserId" value="{$UserId}"/>
       <div class="form-row">
-          <label for="firstName">First Name</label>
-          <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name" required value="{$FirstName}"/>
+          <label for="FirstName">First Name</label>
+          <input type="text" class="form-control" name="FirstName" id="FirstName" placeholder="First Name" required value="{$FirstName}"/>
 	   </div>
 	   <div class="form-row">
-          <label for="lastName">Last Name</label>
-          <input type="text" class="form-control" name="lastName" id="firstName" placeholder="First Name" required value="{$LastName}" />
+          <label for="LastName">Last Name</label>
+          <input type="text" class="form-control" name="LastName" id="LastName" placeholder="Last Name" required value="{$LastName}" />
+	   </div>
+	   <div class="form-row">
+          <label for="Username">Username / Email Address</label>
+          <input type="text" class="form-control" name="Username" id="Username" placeholder="Email Address" required value="{$Username}" />
+	   </div>
+	   <div class="form-row">
+          <label for="Comments">Comments</label>
+          <textarea class="form-control" name="Comments" id="Comments">{$Comments}</textarea>
+	   </div>
+	   <div>
+          <label for="Created">Created</label>
+          <div class="readonly" name="Created" id="Created">{$CreateDate}</div>
+	   </div>
+	   <div class="btn-toolbar">
+          <input class="btn btn-primary btn-space" type="submit" value="Save" id="btn-submit" disabled />
+          <input class="btn btn-secondary btn-space" type="button" value="Cancel" id="btn-cancel" disabled/>
 	   </div>
   </form>
 </div>
 EOT;
 
 $custom_css = <<<EOT
-
+.readonly {
+background-color: #f0f0f0;
+padding: .375rem .75rem;
+}
+.btn-space { margin: 0 5px; }
+.btn { width: 100px; }
 EOT;
 
 $custom_js = <<<EOT
-
+$(function() {
+    var saved_data = $('#userEditForm').serialize();
+    $('#userEditForm').on( 'click keyup', function(e) {
+      $('#btn-submit,#btn-cancel').attr( "disabled", ($(this).serialize() == saved_data));
+    });
+    $('#btn-cancel').on( 'click keyup', function(e) {
+      $('#userEditForm')[0].reset();
+    });
+    $('#userEditForm').on( 'submit', function(e) {
+      e.preventDefault();
+       e.stopPropagation();
+      if (this.checkValidity()) {
+        $.post( "/scripts/ajax_adminEditUser.php", $(this).serialize(), function(data) {
+          if (data.errors.length > 0) {
+            var msg = 'One or more errors were encountered:\\r\\n\\r\\n';
+              for (var i = 0; i < data.errors.length; i++) {
+                msg +=  data.errors[i] + "\\r\\n";
+              }
+              alert( msg );
+              return false;
+            }
+            else if (data.success == 1) {
+              alert( "The user record changes have been saved." );
+            }
+            else alert(data);
+          }, "json");
+      }
+      else {
+        
+        return false;
+      }
+    });
+});
 EOT;
 
 $p_params = [];
---
+++ htdocs/scripts/ajax_adminEditUser.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dwirth
+ * Date: 2/28/2019
+ * Time: 3:26 PM
+ */
+
+require_once( "../../init.php" );
+if(!isset($_SESSION['admin']) || !$_SESSION['admin']) die("unauthorized access");
+
+$response = [];
+$errors = [];
+$success = 0;
+
+$UserId = filter_input( INPUT_POST, 'UserId', FILTER_SANITIZE_NUMBER_INT );
+$FirstName = filter_input( INPUT_POST, 'FirstName', FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_FLAG_NO_ENCODE_QUOTES );
+$LastName = filter_input( INPUT_POST, 'LastName', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+$Username = filter_input( INPUT_POST, 'Username', FILTER_SANITIZE_EMAIL );
+$Comments = filter_input( INPUT_POST, 'Comments', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+
+if (!$UserId) $errors[] = "Missing required parameter: UserId";
+if (!$FirstName) $errors[] = "The First Name field is required.";
+if (!$LastName) $errors[] = "The Last Name field is required.";
+if (!$Username) $errors[] = "The Username / Email Address field is required.";
+
+if (!count($errors)) {
+    $User = new User( $UserId );
+    $exists = User::usernameExists( $Username, $User->Attributes['Username'] );
+    if ($exists) $errors[] = "That Username / Email Address is already in use.";
+    else {
+        $User->update( 'FirstName', $FirstName );
+        $User->update( 'LastName', $LastName );
+        $User->update( 'Username', $Username );
+        $User->update( 'Comments', $Comments );
+        $success = 1;
+    }
+}
+
+$response['errors'] = $errors;
+$response['success'] = $success;
+echo json_encode( $response );
